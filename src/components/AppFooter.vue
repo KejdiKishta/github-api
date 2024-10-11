4c2c625fbf8eb26283495eeb29a324d0b649ac2a
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-10-11 17:28:01 +0200

    add: simple footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,12 +4,17 @@
 
 <template>
     <footer class="bg-black">
-        
+        <div class="card-footer text-center text-white">
+            <span>&copy; {{ new Date().getFullYear() }} All rights reserved...</span>
+        </div>
     </footer>
 </template>
 
 <style scoped lang="scss">
 footer {
     height: 10vh;
+    span {
+        line-height: 10vh;
+    }
 }
 </style>
