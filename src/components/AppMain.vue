43ed04e4d8695911d918e7660257e5888f3e5385
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-10-11 17:14:12 +0200

    add: main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,34 @@
 <script>
+import { store } from '../store';
+import Card from './Card.vue';
 
+export default {
+    components: {
+        Card,
+    },
+    data() {
+        return {
+            store,
+        };
+    },
+};
 </script>
 
 <template>
-    <main class="bg-dark">
-        
+    <main class="bg-dark text-white">
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-12 col-md-4 mb-4" v-for="card in store.results" :key="card.id">
+                    <Card :item="card" />
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
+
 <style scoped lang="scss">
 main {
-    height: 80vh;
+    min-height: 80vh;
 }
 </style>
